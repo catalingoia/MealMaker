6251518dd72ce8c1e0b4fdeeec4cdda75feb73ba
ddbl <daviddeyv@gmail.com>
2023-03-18 23:50:09 +0200

    add avatar bar

--- healthy-slots/src/shared/Avatar.tsx
+++ healthy-slots/src/shared/Avatar.tsx
@@ -1,9 +1,20 @@
 import { Box, Typography } from "@mui/material";
-
+import avatar from "../assets/avatar.png";
 export default function Avatar() {
   return (
-    <Typography sx={{ fontFamily: "Open Sans", paddingTop: "30px" }}>
-      Hello, Username!
-    </Typography>
+    <>
+      <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+        <img src={avatar} style={{ height: "30px", width: "30px" }} />
+        <Typography
+          sx={{
+            fontFamily: "Open Sans",
+            paddingTop: "30px",
+            paddingLeft: "15px",
+          }}
+        >
+          Hello, Username!
+        </Typography>
+      </Box>
+    </>
   );
 }

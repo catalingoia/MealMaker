f444d8829829191e592c9241be36deed485e6807
ddbl <daviddeyv@gmail.com>
2023-03-19 01:27:29 +0200

    add achievements screen

---
+++ healthy-slots/src/Achievements/Achievement.tsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@mui/material";
+
+interface AchievementProps {
+  icon: any;
+  title: string;
+  subtitle: string;
+  objective: number;
+  currentStatus: number;
+}
+
+export default function Achievement(props: AchievementProps) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor:
+          props.currentStatus % props.objective === 0 ? "#8ee3a6" : "#CDCDCD",
+        borderRadius: "20px",
+        width: "90%",
+        height: "78px",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        marginBottom: "16px",
+      }}
+    >
+      <Box sx={{ paddingX: "10px" }}>{props.icon}</Box>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Typography fontWeight={"bold"} fontFamily={"Open Sans"}>
+          {props.title}
+        </Typography>
+        <Typography fontFamily={"Open Sans"}>{props.subtitle}</Typography>
+      </Box>
+    </Box>
+  );
+}
--- healthy-slots/src/Achievements/Achievements.tsx
+++ healthy-slots/src/Achievements/Achievements.tsx
@@ -1,8 +1,12 @@
-import { Typography } from "@mui/material";
-
+import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
+import EggAltOutlinedIcon from "@mui/icons-material/EggAltOutlined";
+import Achievement from "./Achievement";
+import CakeOutlinedIcon from "@mui/icons-material/CakeOutlined";
+import MenuBookOutlinedIcon from "@mui/icons-material/MenuBookOutlined";
+import RamenDiningIcon from "@mui/icons-material/RamenDining";
 export default function Achievements() {
   return (
-    <div>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Typography
         variant="h5"
         sx={{
@@ -16,7 +20,68 @@ export default function Achievements() {
       >
         ACHIEVEMENTS
       </Typography>
-      // Your home page content here
-    </div>
+      <Box
+        sx={{
+          paddingTop: "20px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Achievement
+          icon={<EggAltOutlinedIcon sx={{ fontSize: "40px" }} />}
+          title={"Eggcelent!"}
+          subtitle={"You have made 5 recipes with eggs!"}
+          objective={5}
+          currentStatus={5}
+        />
+        <Achievement
+          icon={<RamenDiningIcon sx={{ fontSize: "40px" }} />}
+          title={"Ramen expert!"}
+          subtitle={"You have cooked ramen 5 times!"}
+          objective={5}
+          currentStatus={5}
+        />
+        <Achievement
+          icon={<MenuBookOutlinedIcon sx={{ fontSize: "40px" }} />}
+          title={"Master Chef!"}
+          subtitle={"You have made 10 recipes!!"}
+          objective={5}
+          currentStatus={5}
+        />
+        <Achievement
+          icon={<CakeOutlinedIcon sx={{ fontSize: "40px" }} />}
+          title={"Cake Boss"}
+          subtitle={"Make 3 desserts"}
+          objective={3}
+          currentStatus={1}
+        />
+        {/* < */}
+        {/* <Card
+          sx={{
+            backgroundColor: "#8ee3a6",
+            borderRadius: "20px",
+          }}
+        >
+          <CardHeader
+            avatar={<EggAltOutlinedIcon />}
+            title="Eggcelent!"
+            sx={{
+              "& .MuiCardHeader-title": {
+                fontFamily: "Open Sans",
+              },
+            }}
+          ></CardHeader>
+          <CardContent>
+            <Typography
+              sx={{ fontFamily: "Open Sans", fontSize: "16px", color: "white" }}
+            >
+              You have made 5 recipes with eggs!
+            </Typography>
+          </CardContent>
+        </Card> */}
+      </Box>
+    </Box>
   );
 }
